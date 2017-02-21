300112a25a654b5eea7291b9008d6881bc8a165f
Jeremy Beagle <beaglej2012@fau.edu>
2017-02-21 18:14:22 -0500

    Completed

--- starter-code/database.js
+++ starter-code/database.js
@@ -33,7 +33,7 @@ class Database {
   }
   // Insert a user
   // user is the object to insert into the collection
-  // callback has two arguments error and result 
+  // callback has two arguments error and result
   insertUser(user, callback = (error, result) => {}){
     this.connect((error, database) => {
       if (error){
@@ -43,13 +43,13 @@ class Database {
         // Implement the query to insert a user
         // user is the document that we want to insert
         // remeber once it's finish to comment callback('Error inserting user');
-        
-        callback('Error inserting user');
+
+        database.collection('users').insertOne(user, callback);
       }
     });
   }
 
-  listUsers(callback = (error, users) => {}) {
+  listUsers(callback) {
     this.connect((error, database) => {
       if (error){
         callback(error);
@@ -57,13 +57,13 @@ class Database {
         //  LAB 2
         // Implement the query to insert a user
         // remeber once it's finish to comment callback('Error listing users');
-        
-        callback('Error listing users');
+
+        database.collection('users').find().toArray(callback);
       }
     });
   }
 
-  deleteUser( firstName, callback = (error, result) => {}) {
+  deleteUser( firstName, callback ) {
     this.connect((error, database) => {
       if (error){
         callback(error);
@@ -72,8 +72,10 @@ class Database {
         // Implement the query to delete a user
         // firstName is the name of user that we want to delete
         // remeber once it's finish to comment callback('Error deleting user');
-        
-        callback('Error deleting user');
+        const filter = {
+          firstName: firstName
+        };
+        database.collection('users').deleteOne(filter, callback);
       }
     });
   }
@@ -87,8 +89,8 @@ class Database {
         // Implement the query to insert a product
         // product is the document to insert
         // remeber once it's finish to comment callback('Error inserting product');
-        
-        callback('Error inserting product');
+
+        database.collection('products').insertOne(product, callback);
       }
     });
   }
@@ -101,8 +103,8 @@ class Database {
         // LAB 5
         // Implement the query to list all products
         // remeber once it's finish to comment callback('Error listing products');
-        
-        callback('Error listing products');
+
+        database.collection('products').find().toArray(callback);
       }
     });
   }
@@ -114,10 +116,12 @@ class Database {
       } else {
         // LAB 6
         // Implement the query to delete a product
-        // productName is the name of the producto to delete 
+        // productName is the name of the producto to delete
         // remeber once it's finish to comment callback('Error deleting product');
-        
-        callback('Error deleting product');
+        const filter = {
+          name: productName
+        };
+        database.collection('products').deleteOne(filter, callback);
       }
     });
   }
@@ -133,8 +137,13 @@ class Database {
         // productName is the name of the product that we want to buy
         // Think if you may need to implement two queries chained
         // remeber once it's finish to comment callback('Error buying product');
-        
-        callback('Error buying product');
+        const filter = {
+          firstName: userFirstName
+        };
+        const updateTarget = {
+          $set: {shoppingCart: productName}
+        };
+        database.collection('users').updateOne(filter, updateTarget, callback);
       }
     });
   }
@@ -149,8 +158,13 @@ class Database {
         // productName is the name of the product to review
         // review is the document to insert
         // remeber once it's finish to comment callback('Error reviewing product');
-        
-        callback('Error reviewing product');
+        const filter = {
+          name: productName
+        };
+        const updateTarget = {
+          reviews: review
+        };
+        database.collection('products').updateOne(filter, updateTarget, callback);
       }
     });
   }
