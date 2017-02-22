67b17d7637e146256637f0aaa455c47a861f62a6
Walter Monecke <wmonecke@gmail.com>
2017-02-22 09:15:16 -0500

    Done

---
+++ app.js
@@ -0,0 +1,23 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const dbServer = 'localhost';
+const dbPort   = '27017';
+const dbName   = 'myShop';
+const connectionURL = `mongodb://${dbServer}:${dbPort}/${dbName}`;
+
+// Use connect method to connect to the server
+MongoClient.connect(connectionURL, function(error, db) {
+  if(error) { console.log("Error connecting to server", error); return;}
+
+  console.log("Connected successfully to server");
+  console.log(db);
+
+  // Make your queries here
+  db.collection('products').find().toArray((error, products) => {
+      // assert.equal(error, null);
+    console.log('All Products: ', products);
+  });
+
+
+  db.close();
+});
