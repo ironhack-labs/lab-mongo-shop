6a4f87814297dc25d2edf34ebb60429e49b4b85a
Rafael Melón <info@rafaelmelon.es>
2017-04-25 17:34:09 +0200

    Primer commit

--- starter-code/database.js
+++ starter-code/database.js
@@ -33,7 +33,7 @@ class Database {
   }
   // Insert a user
   // user is the object to insert into the collection
-  // callback has two arguments error and result 
+  // callback has two arguments error and result
   insertUser(user, callback = (error, result) => {}){
     this.connect((error, database) => {
       if (error){
@@ -43,7 +43,8 @@ class Database {
         // Implement the query to insert a user
         // user is the document that we want to insert
         // remeber once it's finish to comment callback('Error inserting user');
-        
+        database.collection("users").insertOne(user,callback);
+
         callback('Error inserting user');
       }
     });
@@ -57,7 +58,7 @@ class Database {
         //  LAB 2
         // Implement the query to insert a user
         // remeber once it's finish to comment callback('Error listing users');
-        
+
         callback('Error listing users');
       }
     });
@@ -72,7 +73,7 @@ class Database {
         // Implement the query to delete a user
         // firstName is the name of user that we want to delete
         // remeber once it's finish to comment callback('Error deleting user');
-        
+
         callback('Error deleting user');
       }
     });
@@ -87,7 +88,7 @@ class Database {
         // Implement the query to insert a product
         // product is the document to insert
         // remeber once it's finish to comment callback('Error inserting product');
-        
+
         callback('Error inserting product');
       }
     });
@@ -101,7 +102,7 @@ class Database {
         // LAB 5
         // Implement the query to list all products
         // remeber once it's finish to comment callback('Error listing products');
-        
+
         callback('Error listing products');
       }
     });
@@ -114,9 +115,9 @@ class Database {
       } else {
         // LAB 6
         // Implement the query to delete a product
-        // productName is the name of the producto to delete 
+        // productName is the name of the producto to delete
         // remeber once it's finish to comment callback('Error deleting product');
-        
+
         callback('Error deleting product');
       }
     });
@@ -133,7 +134,7 @@ class Database {
         // productName is the name of the product that we want to buy
         // Think if you may need to implement two queries chained
         // remeber once it's finish to comment callback('Error buying product');
-        
+
         callback('Error buying product');
       }
     });
@@ -149,7 +150,7 @@ class Database {
         // productName is the name of the product to review
         // review is the document to insert
         // remeber once it's finish to comment callback('Error reviewing product');
-        
+
         callback('Error reviewing product');
       }
     });
