08e05b799a96e244715d463200aa239c22d95b80
TuckerDS <jjperez.tucker.dev@gmail.com>
2017-04-25 18:04:09 +0200

    Complete

--- starter-code/database.js
+++ starter-code/database.js
@@ -92,8 +92,8 @@ class Database {
         // Implement the query to insert a product
         // product is the document to insert
         // remeber once it's finish to comment callback('Error inserting product');
-
-        callback('Error inserting product');
+        database.collection('products').insertOne(product, callback);
+        //callback('Error inserting product');
       }
     });
   }
@@ -106,8 +106,8 @@ class Database {
         // LAB 5
         // Implement the query to list all products
         // remeber once it's finish to comment callback('Error listing products');
-
-        callback('Error listing products');
+        database.collection('products').find({}, callback);
+        //callback('Error listing products');
       }
     });
   }
@@ -121,8 +121,8 @@ class Database {
         // Implement the query to delete a product
         // productName is the name of the producto to delete
         // remeber once it's finish to comment callback('Error deleting product');
-
-        callback('Error deleting product');
+        database.collection('products').deleteOne({"name": productName}, callback);
+        //callback('Error deleting product');
       }
     });
   }
@@ -138,8 +138,8 @@ class Database {
         // productName is the name of the product that we want to buy
         // Think if you may need to implement two queries chained
         // remeber once it's finish to comment callback('Error buying product');
-
-        callback('Error buying product');
+        database.collection('users').findOneAndUpdate({"firstName": userFirstName}, {$push:{"shoppingCart":productName}}, callback);
+        //callback('Error buying product');
       }
     });
   }
@@ -147,15 +147,15 @@ class Database {
   addReviewToProduct( {productName, review}, callback = (messageResult) => {}){
     this.connect((error, database) => {
       if (error) {
-        callback(error)
+        callback(error);
       } else {
         // LAB 8
         // Implement the query to review a product
         // productName is the name of the product to review
         // review is the document to insert
         // remeber once it's finish to comment callback('Error reviewing product');
-
-        callback('Error reviewing product');
+        database.collection('products').findOneAndUpdate({"name": productName}, {$push:{"reviews":review}}, callback);
+        //callback('Error reviewing product');
       }
     });
   }
