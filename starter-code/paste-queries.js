9ae33d5c9409dae0f930a6e22e6938d6e1f371e1
Lawrence Fitzgerald <lfitzmail@gmail.com>
2017-06-20 16:55:23 -0400

    Updated paste-gueries.js
    Completed first three questions.

--- starter-code/paste-queries.js
+++ starter-code/paste-queries.js
@@ -2,10 +2,68 @@
 // ------------------------------------------------
 // PASTE 3 USER INSERT QUERIES HERE
 
+db.users.insertOne({
+    "firstName": "John",
+    "lastName": "Smith",
+    "dateBirth": ISODate("2016-12-10T18:28:09.369Z"),
+    "address": {
+      "streetAddress": "21 2nd Street",
+      "city": "New York",
+      "state": "NY",
+      "postalCode": "10021"
+    }
+  });
+
+  db.users.insertOne({
+      "firstName": "John",
+      "lastName": "Smith",
+      "dateBirth": ISODate("2016-12-10T18:28:09.369Z"),
+      "address": {
+        "streetAddress": "21 2nd Street",
+        "city": "New York",
+        "state": "NY",
+        "postalCode": "10021"
+      }
+    });
+
+  db.users.insertOne({
+    "firstName": "John",
+    "lastName": "Smith",
+    "dateBirth": ISODate("2016-12-10T18:28:09.369Z"),
+    "address": {
+      "streetAddress": "21 2nd Street",
+      "city": "New York",
+      "state": "NY",
+      "postalCode": "10021"
+    }
+  });
 
 // 1.3 | Insert our first products in products collection
 // ------------------------------------------------------
 // PASTE 3 PRODUCT INSERT QUERIES HERE
+
+db.products.insertOne({
+     "name": "Water Bottle",
+     "description":"High quality glass bottle provides a healthier way to drink.  Silicone sleeve provides a good grip, a see-through window, and protects the glass vessel.  Eliminates toxic leaching that plastic can cause.  Innovative design holds 22-1/2 ounces.  Dishwasher safe",
+     "category":"Kitchen",
+     "price":23.0
+  });
+
+db.products.insertOne(
+  {
+     "name": "Super-Duper Soaker",
+     "description":"More powerful than a water hose!  Warning: Do not spray at small children.",
+     "category":"Toys",
+     "price":230.00
+  });
+
+db.products.insertOne(
+  {
+     "name": "Super-Duper Soaker",
+     "description":"More powerful than a water hose!  Warning: Do not spray at small children.",
+     "category":"Toys",
+     "price":230.00
+  });
 
 
 // 1.4 | Getting Started with queries
@@ -23,4 +81,3 @@
 
 
 // PASTE REVIEW QUERY HERE
-
