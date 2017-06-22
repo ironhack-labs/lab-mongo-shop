aff0e2a15c892aea0f5422d567bdc81eaab0cd86
zocha_sz <zofia.szczudlik@gmail.com>
2017-06-22 17:47:25 +0200

    Done

---
+++ starter-code/apuntes.js
@@ -0,0 +1,50 @@
+{
+  "_id" : ObjectId("59479bb91d2e741d3f7d7a2a"),
+"firstName" : "John",
+"lastName" : "Smith",
+"dateBirth" : ISODate("2016-12-10T18:28:09.369Z"),
+"address" : {
+  "streetAddress" : "21 2nd Street",
+  "city" : "New York",
+  "state" : "NY",
+  "postalCode" : "10021"
+},
+"shoppingCart" : [
+  ObjectId("594536ca1d2e741d3f7d7994")
+]
+
+"firstName" : "Miguel",
+"lastName" : "Perez",
+"dateBirth" : ISODate("1983-11-10T18:28:09.369Z"),
+"address" : {
+  "streetAddress" : "Antonio Toledano 22",
+  "city" : "Madrid",
+  "state" : null,
+  "postalCode" : "28028"
+},
+"shoppingCart" : [ ]
+
+
+
+"firstName" : "Zofia",
+"lastName" : "Szczudlik",
+"dateBirth" : ISODate("1979-02-02T18:28:09.369Z"),
+"address" : {
+  "streetAddress" : "San Ambrosio 8",
+  "city" : "Madrid",
+  "state" : null,
+  "postalCode" : "28011"
+},
+"shoppingCart" : [ ]
+
+
+
+"name": "Blanket",
+"description":"Warm and cosy",
+"category":"Living-room",
+"price":17.0
+
+"name": "Mug",
+"description":"Perfect for your morning coffee",
+"category":"Kitchen",
+"price":9.0
