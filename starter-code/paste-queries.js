7955a6c2f23e22d805ab1cd31218453727d0fa52
Jordi Piella <jordipiella@gmail.com>
2017-07-07 15:20:48 +0200

    Done

--- starter-code/paste-queries.js
+++ starter-code/paste-queries.js
@@ -1,26 +1,64 @@
 // 1.2 | Insert our first users in users collection
 // ------------------------------------------------
 // PASTE 3 USER INSERT QUERIES HERE
-
-
+use MyShop
+db.users.insertOne({
+  "firstName": "John",
+  "lastName": "Smith",
+  "dateBirth": ISODate("2016-12-10T18:28:09.369Z"),
+  "address": {
+    "streetAddress": "21 2nd Street",
+    "city": "New York",
+    "state": "NY",
+    "postalCode": "10021"
+  }
+});
 // 1.3 | Insert our first products in products collection
 // ------------------------------------------------------
 // PASTE 3 PRODUCT INSERT QUERIES HERE
-
+db.products.insertMany([{
+   "name": "Water Bottle",
+   "description":"High quality glass bottle provides a healthier way to drink.  Silicone sleeve provides a good grip, a see-through window, and protects the glass vessel.  Eliminates toxic leaching that plastic can cause.  Innovative design holds 22-1/2 ounces.  Dishwasher safe",
+   "category":"Kitchen",
+   "price":23.0
+},
+{
+   "name": "Red Bottle",
+   "description":"High quality glass bottle provides a healthier way to drink.  Silicone sleeve provides a good grip, a see-through window, and protects the glass vessel.  Eliminates toxic leaching that plastic can cause.  Innovative design holds 22-1/2 ounces.  Dishwasher safe",
+   "category":"Kitchen",
+   "price":23.0
+},
+{
+   "name": "Black Bottle",
+   "description":"High quality glass bottle provides a healthier way to drink.  Silicone sleeve provides a good grip, a see-through window, and protects the glass vessel.  Eliminates toxic leaching that plastic can cause.  Innovative design holds 22-1/2 ounces.  Dishwasher safe",
+   "category":"Kitchen",
+   "price":25.0
+},
+{
+   "name": "Wine Bottle",
+   "description":"High quality glass bottle provides a healthier way to drink.  Silicone sleeve provides a good grip, a see-through window, and protects the glass vessel.  Eliminates toxic leaching that plastic can cause.  Innovative design holds 22-1/2 ounces.  Dishwasher safe",
+   "category":"Kitchen",
+   "price":30.0
+}]);
 
 // 1.4 | Getting Started with queries
 // ----------------------------------
 // PASTE SHOPPING CART QUERY HERE
-
+db.users.update({"firstName": "John"},{$set: {"shoppingCart":[ObjectId("595b7322601cd24673f0f066"), ObjectId("595f7bb00b16c783f2891427"), ObjectId("595f7bb00b16c783f2891427") ]}});
 
 // PASTE LIST PRODUCTS QUERY HERE
-
+db.products.find().pretty();
 
 // PASTE CATEGORY PRODUCTS QUERY HERE
-
+db.products.find({}, {"category": 1, "_id":0});
 
 // PASTE DELETE PRODUCT QUERY HERE
-
+db.products.deleteOne({_id: ObjectId("595f7bb00b16c783f289142a")});
 
 // PASTE REVIEW QUERY HERE
-
+db.products.update({_id: ObjectId("595b7322601cd24673f0f066")},{$set: {"reviews": [{
+      "name": "Shannon",
+      "comment": "This is so warm and comfortable.",
+      "stars": 2,
+      "date": "2016-11-10T18:28:09.369Z"
+    }]}});
