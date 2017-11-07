0d2d6e75a96f16625fdd27b35b7fbb40176e0446
FranRom <maildefranz@gmail.com>
2017-11-07 16:03:35 +0100

    working on the last two methods

--- starter-code/database.js
+++ starter-code/database.js
@@ -57,7 +57,7 @@ class Database {
         //  LAB 2
         // Implement the query to insert a user
         // remeber once it's finish to comment callback('Error listing users');
-        database.collection("users").find();
+        database.collection("users").find({}, callback);
         //callback('Error listing users');
       }
     });
@@ -72,8 +72,8 @@ class Database {
         // Implement the query to delete a user
         // firstName is the name of user that we want to delete
         // remeber once it's finish to comment callback('Error deleting user');
-
-        callback('Error deleting user');
+          database.collection("users").deleteOne({"firstName":firstName}, callback);
+        // callback('Error deleting user');
       }
     });
   }
@@ -87,8 +87,8 @@ class Database {
         // Implement the query to insert a product
         // product is the document to insert
         // remeber once it's finish to comment callback('Error inserting product');
-
-        callback('Error inserting product');
+          database.collection("products").insertOne(product,callback);
+        // callback('Error inserting product');
       }
     });
   }
@@ -101,8 +101,8 @@ class Database {
         // LAB 5
         // Implement the query to list all products
         // remeber once it's finish to comment callback('Error listing products');
-
-        callback('Error listing products');
+        database.collection("products").find({}, callback);
+        // callback('Error listing products');
       }
     });
   }
@@ -116,8 +116,8 @@ class Database {
         // Implement the query to delete a product
         // productName is the name of the producto to delete
         // remeber once it's finish to comment callback('Error deleting product');
-
-        callback('Error deleting product');
+        database.collection("products").deleteOne({"name": productName}, callback);
+        // callback('Error deleting product');
       }
     });
   }
