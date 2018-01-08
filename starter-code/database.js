13334b5d9752d17f49ec603d84398cb6a2f9b217
Guillermo Alarcón <xxmgaxx@gmail.com>
2018-01-08 09:27:39 +0100

    feat: done

--- starter-code/database.js
+++ starter-code/database.js
@@ -41,10 +41,11 @@ class Database {
       } else {
         // LAB 1
         // Implement the query to insert a user
+        database.collection('users').insertOne(user, callback);
         // user is the document that we want to insert
         // remeber once it's finish to comment callback('Error inserting user');
         
-        callback('Error inserting user');
+        // callback('Error inserting user');
       }
     });
   }
@@ -57,8 +58,8 @@ class Database {
         //  LAB 2
         // Implement the query to insert a user
         // remeber once it's finish to comment callback('Error listing users');
-        
-        callback('Error listing users');
+        this.database.collection('users').find({}, callback);
+        // callback('Error listing users');
       }
     });
   }
@@ -72,8 +73,17 @@ class Database {
         // Implement the query to delete a user
         // firstName is the name of user that we want to delete
         // remeber once it's finish to comment callback('Error deleting user');
-        
-        callback('Error deleting user');
+        this.database.collection('users').findOne({'firstName':firstName},(error,user)=>{
+          if(error){
+            callback(error);
+          }else if(!user){
+            let error = 'Error non-existent user'
+            callback(error);
+          }else{
+            this.database.collection('users').deleteOne({'firstName' : firstName}, callback);
+          }
+        });
+        // callback('Error deleting user');
       }
     });
   }
@@ -87,8 +97,9 @@ class Database {
         // Implement the query to insert a product
         // product is the document to insert
         // remeber once it's finish to comment callback('Error inserting product');
+        this.database.collection('products').insertOne(product, callback);
         
-        callback('Error inserting product');
+        // callback('Error inserting product');
       }
     });
   }
@@ -101,8 +112,8 @@ class Database {
         // LAB 5
         // Implement the query to list all products
         // remeber once it's finish to comment callback('Error listing products');
-        
-        callback('Error listing products');
+        this.database.collection('products').find({}, callback);
+        // callback('Error listing products');
       }
     });
   }
@@ -116,8 +127,17 @@ class Database {
         // Implement the query to delete a product
         // productName is the name of the producto to delete 
         // remeber once it's finish to comment callback('Error deleting product');
-        
-        callback('Error deleting product');
+        this.database.collection('products').findOne({'name':productName},(error,product)=>{
+          if(error){
+            callback(error);
+          }else if(!product){
+            let error = 'Product Not Found'
+            callback(error);
+          }else{
+            this.database.collection('products').deleteOne({'name' : productName}, callback)
+          }
+        });
+        // callback('Error deleting product');
       }
     });
   }
@@ -133,8 +153,26 @@ class Database {
         // productName is the name of the product that we want to buy
         // Think if you may need to implement two queries chained
         // remeber once it's finish to comment callback('Error buying product');
-        
-        callback('Error buying product');
+        this.database.collection('users').findOne({'firstName':userFirstName},(error,user)=>{
+          if(error){
+            callback(error);
+          }else if(!user){
+            let error = 'non-existent user'
+            callback(error);
+          }else{
+            this.database.collection('products').findOne({'name':productName},(error,product)=>{
+              if(error){
+                callback(error);
+              }else if(!product){
+                let error = 'Product Not Found'
+                callback(error);
+              }else{
+                this.database.collection('users').updateOne({'firstName':userFirstName},{$push:{'shoppingCart':product._id}},callback);
+              }
+            });
+          }
+        });
+        // callback('Error buying product');
       }
     });
   }
@@ -149,8 +187,18 @@ class Database {
         // productName is the name of the product to review
         // review is the document to insert
         // remeber once it's finish to comment callback('Error reviewing product');
-        
-        callback('Error reviewing product');
+        this.database.collection('products').findOne({'name':productName},(error,product)=>{
+          if(error){
+            callback(error);
+          }else if(!product){
+            let error = 'Product Not Found'
+            callback(error);
+          }else{
+            this.database.collection('products').updateOne({'name' : productName},{$push:{'reviews':review}}, callback)
+          }
+        });
+
+        // callback('Error reviewing product');
       }
     });
   }
