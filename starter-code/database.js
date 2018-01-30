9c2feba9968c648755dc7b542d658c40aa4f3ec4
100fue <malvacien@gmail.com>
2018-01-30 18:21:36 +0100

    we didnt finish writeReview

--- starter-code/database.js
+++ starter-code/database.js
@@ -9,12 +9,12 @@ class Database {
     this.url = `mongodb://${host}/${database}`;
   }
 
-  connect(callback = (error, database) => {}){
-    if (this.database){
+  connect(callback = (error, database) => { }) {
+    if (this.database) {
       callback(null, this.database);
     } else {
       MongoDB.MongoClient.connect(this.url, (error, database) => {
-        if (error){
+        if (error) {
           callback(error);
         } else {
           this.database = database;
@@ -24,8 +24,8 @@ class Database {
     }
   }
 
-  close(callback = (error) => {}){
-    if (this.database){
+  close(callback = (error) => { }) {
+    if (this.database) {
       this.database.close(true, callback);
     } else {
       callback();
@@ -34,95 +34,106 @@ class Database {
   // Insert a user
   // user is the object to insert into the collection
   // callback has two arguments error and result 
-  insertUser(user, callback = (error, result) => {}){
+  insertUser(user, callback = (error, result) => { }) {
     this.connect((error, database) => {
-      if (error){
+      if (error) {
         callback(error);
       } else {
         // LAB 1
         // Implement the query to insert a user
         // user is the document that we want to insert
         // remeber once it's finish to comment callback('Error inserting user');
-        
-        callback('Error inserting user');
+
+        database.collection('users').insertOne(user,error);
+        callback('User added');
       }
     });
   }
 
-  listUsers(callback = (error, users) => {}) {
+  listUsers(callback = (error, users) => { }) {
     this.connect((error, database) => {
-      if (error){
+      if (error) {
         callback(error);
       } else {
         //  LAB 2
-        // Implement the query to insert a user
+        // Implement the query list users
         // remeber once it's finish to comment callback('Error listing users');
-        
-        callback('Error listing users');
+
+        database.collection('users').find().toArray((error,users)=>{
+          console.log(users);
+        });
+
+        //callback('Error listing users');
       }
     });
   }
 
-  deleteUser( firstName, callback = (error, result) => {}) {
+  deleteUser(firstName, callback = (error, result) => { }) {
     this.connect((error, database) => {
-      if (error){
+      if (error) {
         callback(error);
       } else {
         //  LAB 3
         // Implement the query to delete a user
         // firstName is the name of user that we want to delete
         // remeber once it's finish to comment callback('Error deleting user');
-        
-        callback('Error deleting user');
+
+        //ojo las llaves en los arguments!!
+        database.collection('users').deleteOne({firstName,error});
+        callback('User deleted');
       }
     });
   }
 
-  insertProduct(product, callback = (error, result) => {}){
+  insertProduct(product, callback = (error, result) => { }) {
     this.connect((error, database) => {
-      if (error){
+      if (error) {
         callback(error);
       } else {
         // LAB 4
         // Implement the query to insert a product
         // product is the document to insert
         // remeber once it's finish to comment callback('Error inserting product');
-        
-        callback('Error inserting product');
+        database.collection('products').insertOne(product,error);
+        callback('Product added');
+        //callback('Error inserting product');
       }
     });
   }
 
-  listProducts(callback = (error, products) => {}) {
+  listProducts(callback = (error, products) => { }) {
     this.connect((error, database) => {
-      if (error){
+      if (error) {
         callback(error);
       } else {
         // LAB 5
         // Implement the query to list all products
         // remeber once it's finish to comment callback('Error listing products');
-        
-        callback('Error listing products');
+        database.collection('products').find().toArray((error,products)=>{
+          console.log(products);
+        });
+        callback('All products: ');
       }
     });
   }
 
-  deleteProduct( productName, callback = (error, result) => {}) {
+  deleteProduct(name, callback = (error, result) => { }) {
     this.connect((error, database) => {
-      if (error){
+      if (error) {
         callback(error);
       } else {
         // LAB 6
         // Implement the query to delete a product
         // productName is the name of the producto to delete 
         // remeber once it's finish to comment callback('Error deleting product');
-        
-        callback('Error deleting product');
+        database.collection('products').deleteOne({name,error});
+        callback('Product deleted');
+        //callback('Error deleting product');
       }
     });
   }
 
-  addProductToShoppingCart({userFirstName, productName}, callback = (error) => {}){
+  addProductToShoppingCart({ userFirstName, productName }, callback = (error) => { }) {
     this.connect((error, database) => {
       if (error) {
         callback(error);
@@ -133,13 +144,17 @@ class Database {
         // productName is the name of the product that we want to buy
         // Think if you may need to implement two queries chained
         // remeber once it's finish to comment callback('Error buying product');
-        
-        callback('Error buying product');
+
+
+        //OJO AL PUSH
+
+        database.collection('users').updateMany({'firstName':userFirstName},{$push:{'shoppingCart':productName}});
+        callback('Shopping cart updated!');
       }
     });
   }
 
-  addReviewToProduct( {productName, review}, callback = (messageResult) => {}){
+  addReviewToProduct({ productName, review }, callback = (messageResult) => { }) {
     this.connect((error, database) => {
       if (error) {
         callback(error)
@@ -149,8 +164,10 @@ class Database {
         // productName is the name of the product to review
         // review is the document to insert
         // remeber once it's finish to comment callback('Error reviewing product');
-        
+
+        database.collection('products').updateMany({'name':productName},{$set:{'reviews':[review]}});
         callback('Error reviewing product');
+        //no terminado
       }
     });
   }
--- starter-code/paste-queries.js
+++ starter-code/paste-queries.js
@@ -2,25 +2,34 @@
 // ------------------------------------------------
 // PASTE 3 USER INSERT QUERIES HERE
 
+db.users.insertOne{ "firstName": "Beltran", "lastName": "Rengifo", "dateBirth": ISODate("2014-10-10T18:28:09.369Z"), "address": { "streetAddress": "Elfo 32", "city": "Madrid", "state": "MD", "postalCode": "28027" } }
 
 // 1.3 | Insert our first products in products collection
 // ------------------------------------------------------
 // PASTE 3 PRODUCT INSERT QUERIES HERE
+
+db.products.insertOne({ "name": "Bocadillo de Chorizo", "description": "healthy healthy", "category": "Sandwich", "price": 3.5 })
 
 
 // 1.4 | Getting Started with queries
 // ----------------------------------
 // PASTE SHOPPING CART QUERY HERE
 
+db.users.updateOne({ "firstName": "Miguel" }, { $set: { "shoppingCart": [ObjectId("5a7085ff43460f10c1129a9a"), ObjectId("5a70857e1e4c71a03e805450")] } })
 
 // PASTE LIST PRODUCTS QUERY HERE
 
+db.products.find().pretty()
 
 // PASTE CATEGORY PRODUCTS QUERY HERE
 
+db.products.find({ "category": "sodas" }).pretty()
+db.products.find({ "category": "sodas" }).pretty().count()
 
 // PASTE DELETE PRODUCT QUERY HERE
 
+db.products.deleteOne({ "name": "Water Bottle" })
 
 // PASTE REVIEW QUERY HERE
 
+db.products.updateMany({ "name": "Bocadillo de Chorizo" }, { $set: { "reviews": [{ "name": "Pepito", "comment": "Está cojonudo", "stars": "5", "date": "2018-01-20T18:28:09.369Z" }] } })
