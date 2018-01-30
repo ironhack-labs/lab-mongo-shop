55f514302b3b413fb02bda4b5fc77cf2077a3d12
Lucas Martin Calderon <lucasmartincalderon1@gmail.com>
2018-01-30 16:08:07 +0100

    Done two iterations

--- starter-code/paste-queries.js
+++ starter-code/paste-queries.js
@@ -2,11 +2,15 @@
 // ------------------------------------------------
 // PASTE 3 USER INSERT QUERIES HERE
 
+mongoimport --db myShop --collection users --file /Users/lucasmartin/Desktop/lab-mongo-shop/starter-code/data/users.json
+db.users.insertOne(/Users/lucasmartin/Desktop/lab-mongo-shop/starter-code/data/users.json)
 
 // 1.3 | Insert our first products in products collection
 // ------------------------------------------------------
 // PASTE 3 PRODUCT INSERT QUERIES HERE
 
+mongoimport --db myShop --collection products --file /Users/lucasmartin/Desktop/lab-mongo-shop/starter-code/data/products.json
+db.products.insertOne(/Users/lucasmartin/Desktop/lab-mongo-shop/starter-code/data/products.json)
 
 // 1.4 | Getting Started with queries
 // ----------------------------------
