629e2d5525b4ceb910b263f418892211eaf933a4
stephjackson <steph.garrett.jackson@gmail.com>
2018-02-13 12:59:07 -0500

    Halfway functiona - not sure I'm listing items right, things freeze when I add item names, and I'm fairly certain I'm supposed to be adding object IDs to these arrays

--- starter-code/paste-queries.js
+++ starter-code/paste-queries.js
@@ -1,11 +1,52 @@
 // 1.2 | Insert our first users in users collection
 // ------------------------------------------------
-// PASTE 3 USER INSERT QUERIES HERE
+// db.users.insertOne(
+  // {
+  //   "_id": ObjectId("582e3c59eb1b8eb7be087448"),
+  //   "firstName": "John",
+  //   "lastName": "Smith",
+  //   "dateBirth": ISODate("2016-12-10T18:28:09.369Z"),
+  //   "address": {
+  //    "streetAddress": "21 2nd Street",
+  //    "city": "New York",
+  //    "state": "NY",
+  //    "postalCode": "10021"
+  //   },
+  //   "shoppingCart": [
+  //     ObjectId("582e3dd006a0c0b81841f47e"),
+  //     ObjectId("582e3cc7b03b5db7dd616cc8")
+  //   ]
+  // })
 
 
 // 1.3 | Insert our first products in products collection
 // ------------------------------------------------------
-// PASTE 3 PRODUCT INSERT QUERIES HERE
+// db.products.insertOne({
+//   "name": "Water Bottle",
+//   "description":"High quality glass bottle provides a healthier way to drink.  Silicone sleeve provides a good grip, a see-through window, and protects the glass vessel.  Eliminates toxic leaching that plastic can cause.  Innovative design holds 22-1/2 ounces.  Dishwasher safe",
+//   "category":"Kitchen",
+//   "price":23.0
+// })
+// > db.products.insertOne({
+// ...    "name": "WiredTiger T-shirt",
+// ...    "description":"Crafted from ultra-soft combed cotton, this essential t-shirt features sporty contrast tipping and MongoDB's signature leaf.",
+// ...    "category":"Apparel",
+// ...    "reviews": [
+// ...     {
+// ...       "name": "Shannon",
+// ...       "comment": "This is so warm and comfortable.",
+// ...       "stars": 2,
+// ...       "date": "2016-11-10T18:28:09.369Z"
+// ...     },
+// ...     {
+// ...       "name": "Shannon",
+// ...       "comment": "This is so warm and comfortable.",
+// ...       "stars": 3,
+// ...       "date": "2016-11-10T18:28:09.369Z"
+// ...     }
+// ...    ],
+// ...    "price":12.0
+// ... })
 
 
 // 1.4 | Getting Started with queries
