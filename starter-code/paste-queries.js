e84c3241f343ecc940e5e62d928ca02947f70a60
Lexy Vanderford <alexysvanderford@gmail.com>
2018-02-14 09:26:47 -0500

    finished

--- starter-code/paste-queries.js
+++ starter-code/paste-queries.js
@@ -1,15 +1,32 @@
 // 1.2 | Insert our first users in users collection
-// ------------------------------------------------
+
 // PASTE 3 USER INSERT QUERIES HERE
+// {
+//   "firstName": "John",
+//   "lastName": "Smith",
+//   "dateBirth": ISODate("2016-12-10T18:28:09.369Z"),
+//   "address": {
+//     "streetAddress": "21 2nd Street",
+//     "city": "New York",
+//     "state": "NY",
+//     "postalCode": "10021"
+//   }
+// }
 
 
 // 1.3 | Insert our first products in products collection
-// ------------------------------------------------------
+
 // PASTE 3 PRODUCT INSERT QUERIES HERE
+// {
+//     "name": "Water Bottle",
+//     "description":"High quality glass bottle provides a healthier way to drink.  Silicone sleeve provides a good grip, a see-through window, and protects the glass vessel.  Eliminates toxic leaching that plastic can cause.  Innovative design holds 22-1/2 ounces.  Dishwasher safe",
+//     "category":"Kitchen",
+//     "price":23.0
+//  }
 
 
 // 1.4 | Getting Started with queries
-// ----------------------------------
+
 // PASTE SHOPPING CART QUERY HERE
 
 
